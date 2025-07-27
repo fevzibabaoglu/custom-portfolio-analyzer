7c7204b520862ae6c777026c3259af8d1ad645ff
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-07-27 11:12:39 +0300

    feat: implement asset and portfolio data structures

---
+++ src/data_struct/asset.py
@@ -0,0 +1,52 @@
+"""
+custom-portfolio-analyzer - A tool to model, back-test, and compare the performance of your own custom portfolios.
+Copyright (C) 2025  Fevzi Babaoğlu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+from datetime import date
+from typing import List
+
+
+class Price:
+    def __init__(self, date: date, value: float):
+        self.date = date
+        self.value = value
+
+    def get_date(self) -> date:
+        return self.date
+
+    def get_value(self) -> float:
+        return self.value
+
+
+class Asset:
+    def __init__(self, code: str, name: str, prices: List[Price]):
+        self.code = code
+        self.name = name
+        self.prices = prices
+
+    def get_code(self) -> str:
+        return self.code
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_prices(self, start_date: date, end_date: date) -> List[Price]:
+        return [
+            p for p in self.prices
+            if start_date <= p.get_date() <= end_date
+        ]
---
+++ src/data_struct/portfolio.py
@@ -0,0 +1,76 @@
+"""
+custom-portfolio-analyzer - A tool to model, back-test, and compare the performance of your own custom portfolios.
+Copyright (C) 2025  Fevzi Babaoğlu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+from datetime import date
+from typing import List
+
+from .asset import Asset
+
+
+class PortfolioAsset:
+    def __init__(self, asset: Asset, weight: float):
+        self.asset = asset
+        self.weight = weight
+        self._check_validity()
+
+    def get_asset(self) -> Asset:
+        return self.asset
+
+    def get_weight(self) -> float:
+        return self.weight
+
+    def _check_validity(self) -> bool:
+        if not self.get_asset():
+            raise ValueError("Asset cannot be None.")
+
+        if not (0 < self.get_weight() <= 1):
+            raise ValueError(f"Weight {self.get_weight()} for asset {self.get_asset().get_code()} is not valid. Must be between 0 and 1.")
+
+        return True
+
+
+class Portfolio:
+    def __init__(self, title: str, start_date: date, end_date: date, assets: List[PortfolioAsset]):
+        self.title: str = title
+        self.start_date = start_date
+        self.end_date = end_date
+        self.assets = assets
+        self._check_validity()
+
+    def get_title(self) -> str:
+        return self.title
+
+    def get_start_date(self) -> date:
+        return self.start_date
+
+    def get_end_date(self) -> date:
+        return self.end_date
+
+    def get_assets(self) -> List[PortfolioAsset]:
+        return self.assets
+
+    def _check_validity(self) -> bool:
+        if self.get_start_date() >= self.get_end_date():
+            raise ValueError("Start date must be before end date.")
+
+        total_weight = sum(asset.get_weight() for asset in self.get_assets())
+        if total_weight != 1.0:
+            raise ValueError(f"Total weight of assets in portfolio '{self.get_title()}' must equal 1.0, but is {total_weight}.")
+
+        return True
